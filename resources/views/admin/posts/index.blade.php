4bf8326fc72a27520cc9f512183278f0d42ce96f
Juliano Blank <julianoellwanger@gmail.com>
2021-02-01 13:25:46 -0300

    Realizado conexão com o banco e listagem com insert de posts

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,10 @@
+<a href="{{ route('posts.create')}}"> Criar Novo Post </a>
+<hr>
+<h1>Posts</h1>
+
+@foreach ($posts as $post)
+    <p>
+        Titulo: {{$post->title}} <br/>
+        Descrição: {{$post->content}}<br/>
+    </p>    
+@endforeach
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\{
+    PostController
+};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 
 Route::get('/', function () {
     return view('welcome');
